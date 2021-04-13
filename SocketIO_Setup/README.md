c60011886ee04974376edba1efd7cc659237862b
Jeremy Levasseur <jeremy.levasseur.1997@gmail.com>
2021-04-13 12:51:59 -0400

    Added Socket section

---
+++ SocketIO_Setup/README.md
@@ -0,0 +1,5 @@
+# NGINX Docker Setup
+
+To develop the website, edit the HTML, CSS, and JavaScript files within the *web_server/* directory.
+
+The HTML files are located in the *web_server/* directory. The CSS files are located in the *web_server/assets/css/* directory. The JavaScript files are located in the *web_server/assets/js/* directory.
---
+++ SocketIO_Setup/socket_io/assets/js/page1.js
@@ -0,0 +1,29 @@
+// This file contains JavaScript for the index.html file
+
+
+jQuery(document).ready(function ($) {
+
+    console.log("This function runs when the document loads");
+
+    var socket = io();
+
+    var messages = document.getElementById('messages');
+    var form = document.getElementById("form");
+    var input = document.getElementById("input");
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      if (input.value) {
+        socket.emit("chat message", input.value);
+        input.value = "";
+      }
+    });
+
+    socket.on('chat message', function(msg) {
+        var item = document.createElement('li');
+        item.textContent = msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+
+});
