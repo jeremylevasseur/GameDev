732f8b81b2ac575914a6d5cc7d4846d79f1df0b3
Jeremy Levasseur <jeremy.levasseur.1997@gmail.com>
2021-04-13 15:40:40 -0400

    Made a tic tac toe game with Socket IO

--- SocketIO_Setup/socket_io/assets/js/page1.js
+++ SocketIO_Setup/socket_io/assets/js/page1.js
@@ -1,29 +1,209 @@
-// This file contains JavaScript for the index.html file
+/* 
 
 
+  This file contains JavaScript for the index.html file 
+
+
+*/
+
+
+// GLOBAL VARIABLES
+const url = window.location.origin;
+let socket = io.connect(url);
+var basePicturePath = "/img/";
+var myTurn = true;
+var symbol;
+
+// This function runs when the page loads
 jQuery(document).ready(function ($) {
+  
+  // Setting on click functions for the nine separate grid items
+  $("#zero-zero").click(function () {
+    attemptToMakeMove("zero-zero-img");
+  });
 
-    console.log("This function runs when the document loads");
+  $("#zero-one").click(function () {
+    attemptToMakeMove("zero-one-img");
+  });
 
-    var socket = io();
+  $("#zero-two").click(function () {
+    attemptToMakeMove("zero-two-img");
+  });
 
-    var messages = document.getElementById('messages');
-    var form = document.getElementById("form");
-    var input = document.getElementById("input");
+  $("#one-zero").click(function () {
+    attemptToMakeMove("one-zero-img");
+  });
 
-    form.addEventListener("submit", function (e) {
-      e.preventDefault();
-      if (input.value) {
-        socket.emit("chat message", input.value);
-        input.value = "";
-      }
-    });
+  $("#one-one").click(function () {
+    attemptToMakeMove("one-one-img");
+  });
 
-    socket.on('chat message', function(msg) {
-        var item = document.createElement('li');
-        item.textContent = msg;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-    });
+  $("#one-two").click(function () {
+    attemptToMakeMove("one-two-img");
+  });
+
+  $("#two-zero").click(function () {
+    attemptToMakeMove("two-zero-img");
+  });
+
+  $("#two-one").click(function () {
+    attemptToMakeMove("two-one-img");
+  });
+
+  $("#two-two").click(function () {
+    attemptToMakeMove("two-two-img");
+  });
 
 });
+
+
+function getBoardData() {
+  var boardData = {};  // Will contain an object of the current alt values of each grid value
+
+  $(".grid-container img").each(function () {
+    boardData[$(this).attr("id")] = $(this).attr("alt") || "";
+  });
+  /*
+    It will look something like this:
+    {
+      'zero-zero-img': '',
+      'zero-one-img': 'X',
+      'zero-two-img': 'O',
+      ...
+      'two-two-img': 'X',
+    }
+  */
+
+  return boardData;
+
+}
+
+function checkIfGameIsFinished() {
+  var boardData = getBoardData();  // Getting board data
+
+  matches = ["XXX", "OOO"];  // Game is over if one of these matches is made
+
+  // Here is a list of concatenated strings of all the possible winning directions
+  possibleWinningDirections = [
+    boardData['zero-zero-img'] + boardData['zero-one-img'] + boardData['zero-two-img'],
+    boardData['one-zero-img'] + boardData['one-one-img'] + boardData['one-two-img'],
+    boardData['two-zero-img'] + boardData['two-one-img'] + boardData['two-two-img'],
+    boardData['zero-zero-img'] + boardData['one-one-img'] + boardData['two-two-img'],
+    boardData['zero-two-img'] + boardData['one-one-img'] + boardData['two-zero-img'],
+    boardData['zero-zero-img'] + boardData['one-zero-img'] + boardData['two-zero-img'],
+    boardData['zero-one-img'] + boardData['one-one-img'] + boardData['two-one-img'],
+    boardData['zero-two-img'] + boardData['one-two-img'] + boardData['two-two-img']
+  ];
+  /*
+    It will look something like this:
+    ['XOX', 'XXO', 'OOX', ... , 'X']
+
+    The game is over when one of these directions equals 'XXX' or 'OOO'
+  */
+
+  for (var i = 0; i < possibleWinningDirections.length; i++) {
+    if (possibleWinningDirections[i] === matches[0] || possibleWinningDirections[i] === matches[1]) {
+      // Found a match
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Updating the turn message that is displayed on screen
+function updateTurnMessages() {
+  if (!myTurn) {
+    $("#game-status-message").text("Your opponent's turn");
+  } else {
+    $("#game-status-message").text("Your turn.");
+  }
+}
+
+function attemptToMakeMove(imgId) {
+
+  // It is not the player's turn
+  if (!myTurn) {
+    console.log("It is not your turn!");
+    return;
+  }
+
+  // The clicked grid item has already been chosen
+  if ($('#' + imgId).attr("alt").length) {
+    console.log("That space has already been taken!");
+    return;
+  }
+
+  // A move was made and now we need to send the data to the server
+  socket.emit("make.move", {
+    symbol: symbol,
+    position: imgId
+  });
+
+}
+
+// This block is run when a move is made by either player
+socket.on("move.made", function(data) {
+
+  // Setting the grid image to the symbol that was passed by the server
+  $("#" + data.position).attr("src", basePicturePath + data.symbol + ".png");
+  // Setting the alt property of the image to the symbol
+  $("#" + data.position).attr("alt", data.symbol);
+
+  /* 
+    If the symbol recieved from the server is the same as the player's symbol, 
+    that means they just made a move it is no longer their turn.
+  */
+  if (data.symbol === symbol) {
+    myTurn = false;
+  } else {
+    myTurn = true;
+  }
+
+  // Checking to see if the three-in-a-line has been made
+  if (!checkIfGameIsFinished()) {
+
+    // The game is not over yet, move to the next turn
+    updateTurnMessages();
+
+    
+  } else {
+
+    // Changing message to result of the game
+    if (myTurn) {
+      $("#game-status-message").text("Game over. You lost.");
+    } else {
+      $("#game-status-message").text("Game over. You won!");
+    }
+
+    // Disabling the board because the game is over
+    $(".grid-container *").attr("disabled", true).off('click');
+  }
+});
+
+// Setting preconditions when the server gives the game.begin signal
+socket.on("game.begin", function(data) {
+  
+  // Assigning symbol
+  symbol = data.symbol;
+
+  // Assiging image to the symbol statement
+  $('#symbol-statement').attr("src", basePicturePath + data.symbol + ".png");
+
+  // If my symbol is X then I go first
+  if (symbol === "X") {
+    myTurn = true;
+  } else {
+    myTurn = false;
+  }
+
+  updateTurnMessages();
+});
+
+// If the opponent leaves the game, the board needs to be disabled
+socket.on("opponent.left", function() {
+  $("#game-status-message").text("Your opponent left the game.");
+  $(".grid-container *").attr("disabled", "disabled").off('click');
+});
+
+
--- SocketIO_Setup/socket_io/index.js
+++ SocketIO_Setup/socket_io/index.js
@@ -1,29 +1,126 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const server = http.createServer(app);
-const io = require("socket.io")(server);
+const server = http.Server(app).listen(3000);  // Setting the server port to 3000
+const io = require("socket.io")(server);  // Setting up Socket IO
+const fs = require("fs");
 
-app.use(express.static('assets'));
+const clients = {};  // This object will contain all the currently connected users
+
+/*
+  This sets the ./assets/ directory as a static directory
+  that can be accessed through HTTP. The HTML, CSS, JavaScript,
+  and images can be accessed through here.
+*/
+app.use(express.static('assets'));  
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/assets/html/page1.html");
+  const stream = fs.createReadStream(__dirname + "/assets/html/page1.html");
+  stream.pipe(res);
 });
+
+// When a user connects, add them to the client object
+const addClient = socket => {
+  console.log("New client connected", socket.id);
+  clients[socket.id] = socket;
+};
+
+// When a user disconnects, remove them from the client object
+const removeClient = socket => {
+  console.log("Client disconnected", socket.id);
+  delete clients[socket.id];
+};
+
 
 io.on("connection", (socket) => {
 
-  console.log("a user connected");
+  let id = socket.id;
+
+  addClient(socket);
+
+  socket.on("mousemove", data => {
+    data.id = id;
+    socket.broadcast.emit("moving", data);
+  });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    removeClient(socket);
+    socket.broadcast.emit("clientdisconnect", id);
   });
 
 });
 
-server.listen(3000, () => {
-  console.log("Listening on port 3000");
+var players = {};
+var unmatched;
+
+function joinGame(socket) {
+  
+  // Add the player to our object of players
+  players[socket.id] = {
+    opponent: unmatched,
+    symbol: "X",
+    socket: socket
+  };
+
+  /*
+    If the player is unmatched, set their unmatched property to
+    their socket id so that when another player joins, the else
+    block will execute and pair the two.
+  */
+  if (!unmatched) {
+    unmatched = socket.id;
+  } else {
+    players[socket.id].symbol = "O";
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  }
+
+}
+
+// Returns the socket of the opponent
+function getOpponent(socket) {
+
+  if (!players[socket.id].opponent) {
+    return;
+  }
+
+  return players[players[socket.id].opponent].socket;
+}
+
+io.on("connection", function(socket) {
+  joinGame(socket);
+
+  // Once the socket has an opponent, we can begin the game
+  if (getOpponent(socket)) {
+    socket.emit("game.begin", {
+      symbol: players[socket.id].symbol
+    });
+
+    getOpponent(socket).emit("game.begin", {
+      symbol: players[getOpponent(socket).id].symbol
+    });
+  }
+
+  /*
+    Listens for a move to be made and emits an event to both
+    players after the move is completed
+  */
+  socket.on("make.move", function(data) {
+    if (!getOpponent(socket)) {
+      return;
+    }
+
+    // Emit that move made to the two players
+    socket.emit("move.made", data);
+    getOpponent(socket).emit("move.made", data);
+
+  });
+
+  // Emit an event to the opponent when the player leaves
+  socket.on("disconnect", function() {
+    if (getOpponent(socket)) {
+      getOpponent(socket).emit("opponent.left");
+    }
+  });
 });
+
